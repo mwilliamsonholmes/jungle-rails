e06420dc8c2113d00e27df9516b56c328cd46bc7
Melissa <mawilliamson10@gmail.com>
2022-03-15 06:04:01 +0000

    fix bug in sessions controller so now users can log back in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ def new
 end
 
 def create
-
-if user = User.authenticate_with_credentials(params[:email], params[:passsword]) 
+  user = User.find_by_email(params[:email])
+if user = User.authenticate_with_credentials(params[:email], params[:password]) 
    session[:user_id] = user.id
     redirect_to '/'
   else
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-
+  validates :password_digest, presence: true
   validates :password, presence: true, length: { minimum: 8 }
   validates :password_confirmation, presence: true
   
